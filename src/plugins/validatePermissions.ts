c2e91b78169f52487e8ce5038798592285df4717
Luifer1994 <almendralesluifer@gmail.com>
2023-09-28 14:10:02 -0500

    update

--- src/plugins/validatePermissions.ts
+++ src/plugins/validatePermissions.ts
@@ -1,12 +1,36 @@
 //create plugin vue 3
-import {permissions as permissionsUser,userRole as userRoleUser} from "@/utils/validatePermission";
+import { UserLogedInterface } from "@/stores/auth/authInterfaces";
 export default {
-    install: (app, options) => {
-        app.config.globalProperties.$validatePermissions = (permissions: string[]): boolean => {
-            const userPermissions = permissionsUser();
-            const hasPermission = userPermissions.some((p) => permissions.includes(p));
-            return hasPermission;
-        },
-        app.config.globalProperties.$userRole = (): string => userRoleUser();
-    }
-}
+  install: (app, options) => {
+    (app.config.globalProperties.$validatePermissions = (
+      permissions: string[]
+    ): boolean => {
+      const user = JSON.parse(
+        localStorage.getItem("userLoged") || "{}"
+      ) as UserLogedInterface;
+      if (!user) {
+        return false;
+      }
+
+      const permissionsUser = user.permissions;
+      const decode = atob(permissionsUser);
+      const parse = JSON.parse(decode);
+
+      const hasPermission = parse.some((p) => permissions.includes(p));
+      return hasPermission;
+    }),
+    
+      (app.config.globalProperties.$userRole = (): string => {
+        const user = JSON.parse(
+          localStorage.getItem("userLoged") || "{}"
+        ) as UserLogedInterface;
+        if (user && user.roles) {
+          const role = user.roles;
+          const decode = atob(role);
+          const parse = JSON.parse(decode);
+          return parse[0];
+        }
+        return "";
+      });
+  },
+};
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -5,7 +5,8 @@ import { useLoadingStore } from "@/stores/loadingStore";
 
 
 const axiosHttp = axios.create({
-  baseURL: "https://api.districerdott.com/api/",
+  baseURL: "http://127.0.0.1:8000/api/",
+ /*  baseURL: "https://api.districerdott.com/api/", */
 });
 
 axiosHttp.interceptors.request.use(
