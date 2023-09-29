39b6ff0a45fb40780c08f6aff67d51e6b3219773
Luifer1994 <almendralesluifer@gmail.com>
2023-09-29 10:01:58 -0500

    dowload invoice

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -5,8 +5,8 @@ import { useLoadingStore } from "@/stores/loadingStore";
 
 
 const axiosHttp = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
-/*   baseURL: "https://www.districerdott.com/api/", */
+  /* baseURL: "http://127.0.0.1:8000/api/", */
+  baseURL: "https://www.districerdott.com/api/",
 });
 
 axiosHttp.interceptors.request.use(
